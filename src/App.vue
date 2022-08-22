574f370c90956f34cb456eefb3ea621ab14e8786
itelemarked <itelemarked@icloud.com>
2022-08-22 14:38:28 +0200

    basic composition working

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>hellooooo {{name}}!</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const name = ref('Bob')
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
