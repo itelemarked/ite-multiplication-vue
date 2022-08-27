0462db213ced0422ff8785c6ad56055249257ffd
itelemarked <itelemarked@icloud.com>
2022-08-27 22:25:29 +0200

    IonicVue and VueRouter added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { IonicVue } from '@ionic/vue';
 
-createApp(App).mount('#app')
+
+import { deviceService } from './services/device/DeviceService';
+
+const app = createApp(App)
+  .use(IonicVue)
+  .use(router)
+
+router.isReady().then(() => {
+  app.mount('#app');
+});
+
+console.log(deviceService.size);
