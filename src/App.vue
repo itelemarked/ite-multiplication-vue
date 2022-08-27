d2d2f7bd93b05871e24a08fb01bccdee394a8ee5
itelemarked <itelemarked@icloud.com>
2022-08-27 22:27:22 +0200

    add global style file (ionic included)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <TrainingPage/>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
 </template>
 
 <script setup lang="ts">
-  import TrainingPage from "./training/TrainingPage.vue";
+  import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+  @import './styles/ionic-core.css';
+  @import './styles/styles.css';
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+} */
 </style>
