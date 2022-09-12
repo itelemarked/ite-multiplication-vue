e4dfbacf09eb646970d7ffdb4563cd61b8472e30
itelemarked <itelemarked@icloud.com>
2022-09-12 19:30:37 +0200

    Update main files architecture
    index.html in src
    styles.css definition in index.html
    main.ts without css imports
    create a "css" and a "js" folders in order to group dedicated files

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,6 @@ import App from './app/App.vue';
 import router from './app/app-router';
 import { IonicVue } from '@ionic/vue';
 
-// STYLES
-import '@/styles/ionic-core.css';
-import '@/styles/ionic-colors.css';
-import '@/styles/styles.css';
-
 const app = createApp(App).use(IonicVue).use(router);
 
 router.isReady().then(() => {
